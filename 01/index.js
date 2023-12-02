c9ec39a28c87d0b4e34d834563dc28298f93e526
Ryne <ryne@itsrynenotryan.com>
2023-12-01 20:03:22 -0700

    chore(01): Generate array, map down and reduce result

--- 01/index.js
+++ 01/index.js
@@ -1,6 +1,10 @@
 const JFile = require('jfile');
 
-const myF = new JFile('data.txt');
-const array = myF.lines.map((line) => line.replace(/\D+/g, ''));
+const data = new JFile('data.txt');
+const result = data.lines
+  .map((line) => line.replace(/\D+/g, ''))
+  .map((line) => (line.length === 1 ? line + line : line))
+  .map((line) => Number(line.charAt(0) + line.charAt(line.length - 1)))
+  .reduce((a, b) => a + b);
 
-console.log(array);
+console.log(result);
