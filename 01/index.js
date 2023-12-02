8864cac25646b501ab2e6859e683aa9c5c8d7338
Ryne <ryne@itsrynenotryan.com>
2023-12-01 20:45:10 -0700

    chore(01): Part 2 - Convert number substrings to numbers

--- 01/index.js
+++ 01/index.js
@@ -1,7 +1,35 @@
 const JFile = require('jfile');
 
 const data = new JFile('data.txt');
+const digitsMap = {
+  oneight: '18',
+  twone: '21',
+  eightwo: '82',
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
+};
+
+function convertSubstring(inputString) {
+  // Iterate through the keys (substrings) in the digitsMap
+  for (const key in digitsMap) {
+    if (digitsMap.hasOwnProperty(key)) {
+      // Replace occurrences of the key with its corresponding value in the inputString
+      inputString = inputString.split(key).join(digitsMap[key]);
+    }
+  }
+
+  return inputString;
+}
+
 const result = data.lines
+  .map((line) => convertSubstring(line))
   .map((line) => line.replace(/\D+/g, ''))
   .map((line) => (line.length === 1 ? line + line : line))
   .map((line) => Number(line.charAt(0) + line.charAt(line.length - 1)))
