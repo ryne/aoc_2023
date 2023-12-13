afb4a9778b1b660c1edcd9179e4f91329b43e80e
ryne <thejamespittman@gmail.com>
2023-12-13 06:50:12 -0700

    feat(04): ⭐ Part 1

---
+++ 04/index.ts
@@ -0,0 +1,45 @@
+import { loadPuzzleInput } from '../helpers';
+const inputArray = await loadPuzzleInput('input.txt');
+
+// Part 1
+
+type ScratchCard = {
+  cardNumbers: number[];
+  winningNumbers: number[];
+  points: number | null;
+};
+
+const checkScratchCards: ScratchCard[] = inputArray.map((card: string) => {
+  const scratchCard: ScratchCard = {
+    cardNumbers: [],
+    winningNumbers: [],
+    points: null,
+  };
+
+  const formatCards = card
+    .split(':')[1]
+    .trim()
+    .replace(/  +/g, ' ')
+    .split(' | ');
+
+  scratchCard.cardNumbers = formatCards[0].split(' ').map(Number);
+  scratchCard.winningNumbers = formatCards[1].split(' ').map(Number);
+
+  let matches = 0;
+  let points = 0;
+
+  scratchCard.cardNumbers.forEach((number) => {
+    if (scratchCard.winningNumbers.includes(number)) {
+      matches++;
+      matches === 0 || matches === 1 ? points++ : (points = points * 2);
+    }
+  });
+
+  scratchCard.points = points;
+
+  return scratchCard;
+});
+
+const totalPoints = checkScratchCards.reduce((a, c) => a + (c.points ?? 0), 0);
+
+console.log(totalPoints);
